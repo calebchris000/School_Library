5ba5eec9dcf35a40d413f4f5cf0bf5fe18a0cfb0
Caleb Nwaizu <caleb.nwaizu@gurugeeksroyalty.biz>
2023-05-16 13:17:20 +0100

    Add the correct_name method

--- person_class.rb
+++ person_class.rb
@@ -1,24 +1,27 @@
-require './nameable.rb'
+require './nameable'
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, _name = 'Unknown', _parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = nil
-    @name = _name
+    @name = name
     @age = age
-    @parent_permission = _parent_permission
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
     return false unless of_age? || parent_permission
   end
 
-  private def of_age?
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
     return false unless @age >= 18
   end
-  def correct_name
-    return @name
-  end
 end
-
