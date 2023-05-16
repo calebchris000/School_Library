fd235107bfaa6dad0acd67167848c70e199820f8
Caleb Nwaizu <caleb.nwaizu@gurugeeksroyalty.biz>
2023-05-16 13:16:45 +0100

    Module to test the objects

--- person_class.rb
+++ person_class.rb
@@ -1,21 +1,24 @@
-class Person
+require './nameable.rb'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', _parent_permission: true)
+  def initialize(age, _name = 'Unknown', _parent_permission = true)
     @id = nil
-    @name = name
+    @name = _name
     @age = age
-    @parent_permission = parent_permission
+    @parent_permission = _parent_permission
   end
 
   def can_use_services?
     return false unless of_age? || parent_permission
   end
 
-  private
-
-  def of_age?
+  private def of_age?
     return false unless @age >= 18
   end
+  def correct_name
+    return @name
+  end
 end
+
