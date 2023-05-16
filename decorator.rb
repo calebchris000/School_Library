7ede546075626d6eb62760688cd77fa39f232bb7
Caleb Nwaizu <caleb.nwaizu@gurugeeksroyalty.biz>
2023-05-16 13:17:01 +0100

    The decorator class

---
+++ decorator.rb
@@ -0,0 +1,23 @@
+require './nameable'
+
+class Decorator
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.slice(0, 10)
+  end
+end
