db59d96da2fad65b508cf9cb2e030ca6d1036dfd
Caleb Nwaizu <caleb.nwaizu@gurugeeksroyalty.biz>
2023-05-18 16:04:56 +0100

    Complete project

--- person_class.rb
+++ person_class.rb
@@ -1,6 +1,7 @@
 require './nameable'
+require './rental'
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -23,5 +25,9 @@ class Person < Nameable
 
   def of_age?
     return false unless @age >= 18
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 end
